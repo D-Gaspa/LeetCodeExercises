e559b66ee2e066cab3659dc66afa4cdb07e3e0d4
Diego Gasparis Escobedo <diego.gaspariseo@udlap.mx>
2024-06-01 17:51:41 -0600

    Update problem statement for Week 1, June 1st - June 2nd, 2024
    The problem statement for Week 1: June 1st - June 2nd, 2024 was updated in the Jupyter notebook and corresponding python file. It contains now a clearly defined problem, related to the score of a string based on ASCII values. Unnecessary content was removed to keep the file concise and clear.

--- DailyQuestions/2024/6. Jun/Code/Jun01-02.py
+++ DailyQuestions/2024/6. Jun/Code/Jun01-02.py
@@ -3,23 +3,27 @@ from typing import List
 
 from tabulate import tabulate
 
-# Week X: Month day th - Month day th, 2024
+# Week 1: June 1st - June 2nd, 2024
 
-# <------------------------------------------------ Mont day th, 2024 ------------------------------------------------>
-# 1. Problem
+# <-------------------------------------------------- June 1st, 2024 -------------------------------------------------->
+# 3110. Score of a String
 
-# Description
-
-
-def problem1_1():
-    pass
+# Given a string s, the score of the string is the sum of the scores of all of its substrings.
+# The score of a substring is the number of 'a' characters in the substring, multiplied by the length of the substring.
+# Return the score of the string s.
 
 
-def problem1_2():
-    pass
+def scoreOfString(s: str) -> int:
+    n = len(s)
+    total_score = 0
+
+    for index in range(n - 1):
+        total_score += abs((ord(s[index]) - ord(s[index + 1])))
+
+    return total_score
 
 
-# <------------------------------------------------ Mont day th, 2024 ------------------------------------------------>
+# <-------------------------------------------------- June 2nd, 2024 -------------------------------------------------->
 # 2. Problem
 
 # Description
@@ -33,71 +37,3 @@ def problem2_2():
     pass
 
 
-# <------------------------------------------------ Mont day th, 2024 ------------------------------------------------>
-# 3. Problem
-
-# Description
-
-
-def problem3_1():
-    pass
-
-
-def problem3_2():
-    pass
-
-
-# <------------------------------------------------ Mont day th, 2024 ------------------------------------------------>
-# 4. Problem
-
-# Description
-
-
-def problem4_1():
-    pass
-
-
-def problem4_2():
-    pass
-
-
-# <------------------------------------------------ Mont day th, 2024 ------------------------------------------------>
-# 5. Problem
-
-# Description
-
-
-def problem5_1():
-    pass
-
-
-def problem5_2():
-    pass
-
-
-# <------------------------------------------------ Mont day th, 2024 ------------------------------------------------>
-# 6. Problem
-
-# Description
-
-
-def problem6_1():
-    pass
-
-
-def problem6_2():
-    pass
-
-
-# <------------------------------------------------ Mont day th, 2024 ------------------------------------------------>
-# 7. Problem
-
-# Description
-
-
-def problem7_1():
-    pass
-
-
-def problem7_2():
-    pass
