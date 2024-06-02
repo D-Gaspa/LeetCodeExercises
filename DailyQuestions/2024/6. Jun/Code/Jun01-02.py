cdba49ae26190d80c09b4e5c54eeac8a70f48ecd
Diego Gasparis Escobedo <diego.gaspariseo@udlap.mx>
2024-06-01 20:17:20 -0600

    Refactor function scoreOfString and add comments
    The function scoreOfString1 has been refactor to scoreOfString. A corresponding docstring has been added to the function for detailed explanation of its functionality, time, and space complexity. Furthermore, the alternative approach for generating a pair using itertools has been explained.

--- DailyQuestions/2024/6. Jun/Code/Jun01-02.py
+++ DailyQuestions/2024/6. Jun/Code/Jun01-02.py
@@ -14,7 +14,19 @@ from tabulate import tabulate
 # Given a string s, return the score of s.
 
 
-def scoreOfString1(s: str) -> int:
+def scoreOfString(s: str) -> int:
+    """
+    Calculates the total score of a string based on absolute differences between adjacent character ASCII values.
+
+    This function iterates through each pair of adjacent characters in the string,
+    calculates the absolute difference between their ASCII values, and accumulates this into a total score.
+    The final score represents the sum of these absolute differences,
+    providing a measure of the string's overall character variation.
+
+    The time complexity of this function is O(n), where n is the length of the string.
+    This is because it performs a single iteration over the string's characters.
+    The space complexity is O(1) as it uses a constant amount of extra space to store variables.
+    """
     n = len(s)
     total_score = 0
 
@@ -22,7 +34,8 @@ def scoreOfString1(s: str) -> int:
         total_score += abs((ord(s[index]) - ord(s[index + 1])))
 
     # Alternatively, you can use pairwise from itertools to get the pairs of adjacent characters
-    # s -> (s0, s1), (s1, s2), (s2, s3), ...
+    # s -> (s0, s1), (s1, s2), (s2, s3), ... This alternative approach would have similar time and space
+    # complexities, but offers a more concise way of generating pairs
 
     return total_score
 
