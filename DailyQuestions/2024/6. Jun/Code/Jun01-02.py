eb68ee11b7fa863ce7cc07a5f7f8d16746867572
Diego Gasparis Escobedo <diego.gaspariseo@udlap.mx>
2024-06-01 18:02:43 -0600

    Fix typography errors and modify problem descriptions
    Fixed typography errors in the DailyQuestions/2024/6. Jun/Code/Jun03-09.py file by correcting "Mont" to "Month". Also added test case sections for each problem. Modified problem descriptions and the "scoreOfString" function in the DailyQuestions/2024/6. Jun/Code/Jun01-02.py file for more clarity and comprehensibility.

--- DailyQuestions/2024/6. Jun/Code/Jun01-02.py
+++ DailyQuestions/2024/6. Jun/Code/Jun01-02.py
@@ -3,22 +3,26 @@ from typing import List
 
 from tabulate import tabulate
 
+
 # Week 1: June 1st - June 2nd, 2024
 
 # <-------------------------------------------------- June 1st, 2024 -------------------------------------------------->
 # 3110. Score of a String
 
-# Given a string s, the score of the string is the sum of the scores of all of its substrings.
-# The score of a substring is the number of 'a' characters in the substring, multiplied by the length of the substring.
-# Return the score of the string s.
+# The score of a string is defined as the sum of the absolute difference between the ASCII values
+# of adjacent characters.
+# Given a string s, return the score of s.
 
 
-def scoreOfString(s: str) -> int:
+def scoreOfString1(s: str) -> int:
     n = len(s)
     total_score = 0
 
     for index in range(n - 1):
         total_score += abs((ord(s[index]) - ord(s[index + 1])))
+
+    # Alternatively, you can use pairwise from itertools to get the pairs of adjacent characters
+    # s -> (s0, s1), (s1, s2), (s2, s3), ...
 
     return total_score
 
@@ -35,5 +39,3 @@ def problem2_1():
 
 def problem2_2():
     pass
-
-
