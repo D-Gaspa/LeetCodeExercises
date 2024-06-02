c522209414ada368dcd38c387c0348f20bb2f14d
Diego Gasparis Escobedo <diego.gaspariseo@udlap.mx>
2024-06-02 12:56:16 -0600

    Added boilerplate code for Monthly Problems
    Created boilerplate code for the Monthly Problems in the Year 2024. These include placeholder functions for each problem and their corresponding blank test cases for February till December.

---
+++ WeeklyPremium/2024/Code/2. Feb.py
@@ -0,0 +1,88 @@
+from pprint import pprint
+from typing import List
+
+from tabulate import tabulate
+
+# Month X, 2024
+
+# <-------------------------------------------------- Week 1, Month -------------------------------------------------->
+# 1. Problem
+
+# Description
+
+
+def problem1_1():
+    pass
+
+
+def problem1_2():
+    pass
+
+
+# <-------------------------------------------------- Week 2, Month -------------------------------------------------->
+# 2. Problem
+
+# Description
+
+
+def problem2_1():
+    pass
+
+
+def problem2_2():
+    pass
+
+
+# <-------------------------------------------------- Week 3, Month -------------------------------------------------->
+# 3. Problem
+
+# Description
+
+
+def problem3_1():
+    pass
+
+
+def problem3_2():
+    pass
+
+
+# <-------------------------------------------------- Week 4, Month -------------------------------------------------->
+# 4. Problem
+
+# Description
+
+
+def problem4_1():
+    pass
+
+
+def problem4_2():
+    pass
+
+
+# <-------------------------------------------------- Week 5, Month -------------------------------------------------->
+# 5. Problem
+
+# Description
+
+
+def problem5_1():
+    pass
+
+
+def problem5_2():
+    pass
+
+
+# <---------------------------------------------------- Test cases ---------------------------------------------------->
+
+# Test cases for Week 1, Month
+
+# Test cases for Week 2, Month
+
+# Test cases for Week 3, Month
+
+# Test cases for Week 4, Month
+
+# Test cases for Week 5, Month
