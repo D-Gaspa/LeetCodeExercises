b31a2072b98d38226e37c56fb77c8e0b7e265e28
Diego Gasparis Escobedo <diego.gaspariseo@udlap.mx>
2024-06-02 22:34:37 -0600

    Update WeeklyPremium code for June 2024
    This commit modifies the weekly premium problem-solving code for June 2024. Changes include renaming from generic 'Month' to 'June', and updating problem descriptions and function headers to reflect the specific problems to be solved during this month. Test cases sections were also updated accordingly.

--- WeeklyPremium/2024/Code/6. Jun.py
+++ WeeklyPremium/2024/Code/6. Jun.py
@@ -3,23 +3,24 @@ from typing import List
 
 from tabulate import tabulate
 
-# Month X, 2024
+# June, 2024
 
-# <-------------------------------------------------- Week 1, Month -------------------------------------------------->
-# 1. Problem
+# <--------------------------------------------------- Week 1, June --------------------------------------------------->
+# 1940. Longest Common Subsequence Between Sorted Arrays
 
-# Description
+# Given an array of integer arrays `arrays` where each `arrays[i]` is sorted in strictly increasing order,
+# return an integer array representing the longest common subsequence between all the arrays.
 
 
-def problem1_1():
+def longestCommonSubsequence1(arrays: List[List[int]]) -> List[int]:
     pass
 
 
-def problem1_2():
+def longestCommonSubsequence2(arrays: List[List[int]]) -> List[int]:
     pass
 
 
-# <-------------------------------------------------- Week 2, Month -------------------------------------------------->
+# <--------------------------------------------------- Week 2, June --------------------------------------------------->
 # 2. Problem
 
 # Description
@@ -33,7 +34,7 @@ def problem2_2():
     pass
 
 
-# <-------------------------------------------------- Week 3, Month -------------------------------------------------->
+# <--------------------------------------------------- Week 3, June --------------------------------------------------->
 # 3. Problem
 
 # Description
@@ -47,7 +48,7 @@ def problem3_2():
     pass
 
 
-# <-------------------------------------------------- Week 4, Month -------------------------------------------------->
+# <--------------------------------------------------- Week 4, June --------------------------------------------------->
 # 4. Problem
 
 # Description
@@ -61,7 +62,7 @@ def problem4_2():
     pass
 
 
-# <-------------------------------------------------- Week 5, Month -------------------------------------------------->
+# <--------------------------------------------------- Week 5, June --------------------------------------------------->
 # 5. Problem
 
 # Description
@@ -77,12 +78,12 @@ def problem5_2():
 
 # <---------------------------------------------------- Test cases ---------------------------------------------------->
 
-# Test cases for Week 1, Month
+# Test cases for Week 1, June
 
-# Test cases for Week 2, Month
+# Test cases for Week 2, June
 
-# Test cases for Week 3, Month
+# Test cases for Week 3, June
 
-# Test cases for Week 4, Month
+# Test cases for Week 4, June
 
-# Test cases for Week 5, Month
+# Test cases for Week 5, June
